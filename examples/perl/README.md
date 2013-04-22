a79b338d445af8ca2b0154d546044b38613eb67c
Frank T. Bergmann <fbergman@caltech.edu>
2013-04-22 11:00:19 +0200

    add a first perl example

---
+++ examples/perl/README.md
@@ -0,0 +1,17 @@
+## Perl LibSedML Examples
+
+Examples in this folder contain basic perl examples that demonstrate the use of the libSedML library using perl. In order to run them you need to ensure that LibSEDML can be found. As I am not regularly using PERL, I usually do that by specifying the full path where i installed the library in my perl files, like so
+
+	use lib '/usr/local/lib/perl5/site_perl';
+
+
+The following examples are present: 
+
+### echo_sedml.pl
+This basic example, just echos a SED-ML document, reading it and writing it again. It takes two arguments, an input file and the output file. 
+
+### create_sedml.pl
+This example constructs an SED-ML document using the Perl API. It takes one argument, the output file name.
+
+### print_sedml.pl
+This example loads a given SED-ML document and prints an overview of its contents. It takes one argument, the SED-ML document to open. 
---
+++ examples/perl/echo_sedml.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+# -*-Perl-*-
+## 
+## @file    echo_sedml.pl
+## @brief   Echos (and prints) a SED-ML model.
+## @author  Frank T. Bergmann
+## 
+## <!--------------------------------------------------------------------------
+## This file is part of libSEDML.  Please visit http://sed-ml.org for more
+## information about SEDML, and the latest version of libSEDML.
+## 
+## Copyright (c) 2013, Frank T. Bergmann  
+## All rights reserved.
+## 
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met: 
+## 
+## 1. Redistributions of source code must retain the above copyright notice, this
+##    list of conditions and the following disclaimer. 
+## 2. Redistributions in binary form must reproduce the above copyright notice,
+##    this list of conditions and the following disclaimer in the documentation
+##    and/or other materials provided with the distribution. 
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+## ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+## DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+## ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+## LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+## ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+## SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+## ------------------------------------------------------------------------ -.
+## 
+
+use File::Basename;
+use LibSEDML;
+no strict;
+
+if ($#ARGV != 1) {
+   print  "usage: echo_sedml.pl input-filename output-filename\n";
+   exit 1;
+}
+
+$infile  = $ARGV[0];
+$outfile = $ARGV[1];
+
+unless (-e $infile) {
+  print("[Error] ", $infile, ": No such file.", "\n");
+  exit 1;
+}
+
+$d = $LibSBML::readSedML($infile);
+if ( $d->getErrorLog()->getNumFailsWithSeverity($LibSEDML::LIBSEDML_SEV_ERROR) > 0)
+{
+  print ($d->getErrorLog()->toString());
+}
+else
+{
+  $LibSEDML::writeSedML(d, $outfile);
+}
